1b1113c9dbebff69c80b38b0d2c673187753c85c
Juho Rinta-Paavola <juho.rinta-paavola@aalto.fi>
2018-08-29 13:47:22 +0300

    Implement HTTP server

--- o1_auto_tag.py
+++ o1_auto_tag.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import logging
-from threading import Timer
 from aplus_client.client import AplusTokenClient
 from argparse import ArgumentParser
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from itertools import chain
+from threading import Timer
+from urllib.parse import parse_qs
 
 CONFIG = {
     'base_url': 'http://localhost:8000/api/v2/',
@@ -14,14 +16,15 @@ CONFIG = {
     'taggings_query': '?tag_id=',
     'course_id': 3,
     'exercise_ids': {2262, 2263},
-    'timer_interval': 15.0 * 60,
+    'server_address': ('', 8888),
+    'wait_after_post': 5,
 }
 
 tag_for_form_value = {
     'kielitaito': {
-        'suomi': { 'id': 42, 'slug': 'fi' },
-        'ruotsi': { 'id': 44, 'slug': 'sv' },
-        'englanti': { 'id': 43, 'slug': 'en' },
+        'suomi': 'fi',
+        'ruotsi': 'sv',
+        'englanti': 'en',
     },
 }
 
@@ -37,58 +40,56 @@ if args.verbose:
 api = AplusTokenClient(args.token)
 api.set_base_url_from(CONFIG['base_url'])
 
-def get_from_api(url_format, ids):
-    urls = [url_format.format(**CONFIG, id=id) for id in ids]
-    responses = [api.load_data(url) for url in urls]
-    return frozenset(chain(*responses))
+def get_submission(submission_id):
+    return api.load_data('{submissions_url}{submission_id}/'
+                         .format(**CONFIG, submission_id=submission_id))
 
-def get_submissions():
-    return get_from_api('{exercises_url}{id}{submissions_url}',
-                        CONFIG['exercise_ids'])
+def add_tagging(exercise_id, submission_id):
+    if exercise_id not in CONFIG['exercise_ids']:
+        return
 
-def get_taggings():
-    return get_from_api(
-        '{courses_url}{course_id}{taggings_url}{taggings_query}{id}',
-        (tag['id']
-         for field in tag_for_form_value.values()
-         for tag in field.values()))
+    submission = get_submission(submission_id)
+    submitters = submission['submitters']
+    submission_data = submission['submission_data']
 
-def main():
-    Timer(CONFIG['timer_interval'], main).start()
-    submissions = get_submissions()
-    taggings = get_taggings()
-    tagged_user_ids = {tag['user']['id'] for tag in taggings}
+    tag_slugs = (tag_for_form_value[field[0]][field[1]]
+                 for field in submission_data
+                 if field[0] in tag_for_form_value)
+    user_ids = (submitter['id'] for submitter in submitters)
+    post_dataset = (
+        {
+            'user': {
+                'id': user_id,
+            },
+            'tag': {
+                'slug': tag_slug,
+            },
+        }
+        for user_id in user_ids
+        for tag_slug in tag_slugs)
 
-    # Known bug:  if someone/something else has already added a tagging that may be
-    #             given by this excercise, nothing will be done.
-    # Workaround: assign the specified tags using this script *ONLY*
-    submissions_not_tagged = (submission
-                              for submission in submissions
-                              if not tagged_user_ids.issuperset(
-                                      submitter['id']
-                                      for submitter in submission['submitters']))
+    for data in post_dataset:
+        api.do_post('{courses_url}{course_id}{taggings_url}'
+                    .format(**CONFIG), json=data)
 
-    for submission in submissions_not_tagged:
-        submission_data = submission['submission_data']
-        tag_slugs = (tag_for_form_value[field[0]][field[1]]['slug']
-                     for field in submission_data
-                     if field[0] in tag_for_form_value)
-        user_ids = (submitter['id'] for submitter in submission['submitters'])
-        post_dataset = (
-            {
-                'user': {
-                    'id': user_id,
-                },
-                'tag': {
-                    'slug': tag_slug,
-                },
-            }
-            for user_id in user_ids
-            for tag_slug in tag_slugs)
+class APlusCourseHookHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = parse_qs(self.rfile.read(content_length).decode('utf-8'))
+        exercise_id, *_ = (int(id) for id in post_data['exercise_id'])
+        submission_id, *_ = (int(id) for id in post_data['submission_id'])
+        # Wait before making requests to A+, because the submission is not ready
+        # to be read from the API when A+ calls the hook
+        Timer(CONFIG['wait_after_post'],
+              add_tagging,
+              (exercise_id, submission_id)).start()
+        self.send_response(204)
 
-        for data in post_dataset:
-            api.do_post('{courses_url}{course_id}{taggings_url}'
-                        .format(**CONFIG), json=data)
+def run(server_class=HTTPServer,
+        handler_class=APlusCourseHookHTTPRequestHandler):
+    httpd = server_class(CONFIG['server_address'], handler_class)
+    httpd.serve_forever()
+    httpd.server_close()
 
 if __name__ == '__main__':
-    main()
+    run()
